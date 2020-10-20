7e271279dfe2468260fb5c61eaf30ccba7e64d61
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-20 23:21:36 +0900

    新規登録、ログイン機能の実装、挙動確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_data])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password])
   end
 end
