9f441122503d15a05c5fcfda80989ef04e04a6e0
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-21 16:16:50 +0900

    ログインの是非によるページ遷移の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
   end
+
+  def new
+    @product = Product.new 
+  end
+
+  def create
+  end
+
 end
