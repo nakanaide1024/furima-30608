56240fa55eec0f84057d446e8ca5058ac7c1b825
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-21 11:36:23 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true,
-                         format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :last_name, presence: true,
-                        format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}             
-  validates :first_name_kana, presence: true, format: {with:/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
-  validates :last_name_kana, presence: true, format: {with:/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
-  validates :birth_date, presence: true
+  with_options presence: true do 
+    validates :nickname
+    validates :first_name, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :last_name, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}             
+    validates :first_name_kana, format: {with:/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
+    validates :last_name_kana, format: {with:/[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/}
+    validates :birth_date
   
-  validates :email, presence: true,
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true,
-                       length: { minimum: 6 },
-                       format: { with: /\A[a-z0-9]+\z/i },
-                       confirmation: true
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 6 },
+                           format: { with: /\A[a-z0-9]+\z/i },
+                           confirmation: true
+  end                         
 end
