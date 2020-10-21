b42597b77f9df35209e6a9eb1bccdea70397062b
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-21 19:26:04 +0900

    ActiveHashの実装とビューの表示

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_charger
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_date
+
 
   belongs_to :User
 
@@ -6,10 +13,19 @@ class Product < ApplicationRecord
     validates :image
     validates :name
     validates :description
+    validates :category
+    validates :condition
+    validates :delivery_charger
+    validates :prefecture
+    validates :delivery_date
+    validates :price, format: { with:/\A[0-9]+\z/}
+  end
+
+  with_options numericality: { other_then: 1 } do
     validates :category_id
+    validates :condition_id
     validates :delivery_charger_id
     validates :prefecture_id
     validates :delivery_date_id
-    validates :price
   end
 end
