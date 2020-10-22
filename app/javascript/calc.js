812af87668c38e5ccd4f530f8aed1df7c89cfd08
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-22 11:34:31 +0900

    javascriptによる価格表示

---
+++ app/javascript/calc.js
@@ -0,0 +1,13 @@
+function calc (){
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  itemPrice.addEventListener("keyup", () => {
+    priceNum = itemPrice.value;
+    let num = 10;
+    tax = priceNum / num;
+    addTaxPrice.innerHTML = `${tax}`;
+    profit.innerHTML = `${priceNum - tax}`;
+  });
+}
+window.addEventListener("load", calc);
