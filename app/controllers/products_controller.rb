1a79c3476aa4beade065b075f6fff91bd9ee2cb0
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-10-24 20:46:19 +0900

    before_actionの順番入れ替え

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :destroy]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @products = Product.includes(:user).order('created_at DESC')
